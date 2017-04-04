dcc7d04b85ee2cabf91cb409b0f3c857dd609355
scotthartbti <scott.hart.bti@gmail.com>
2017-04-03 21:33:29 -0400

    update dalvik and hwui values
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- device.mk
+++ device.mk
@@ -80,9 +80,9 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=384m \
-    dalvik.vm.heapsize=1024m \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=4m \
     dalvik.vm.heapmaxfree=8m
